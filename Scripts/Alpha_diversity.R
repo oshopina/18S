04e3682c80b72c36c2f004da80a7bcbb59bfd4da
Olga Shopina <uqoshopi@uq.edu.au>
2023-05-30 13:18:48 +1000

    Some cleaning to the script

--- Scripts/Alpha_diversity.R
+++ Scripts/Alpha_diversity.R
@@ -4,14 +4,16 @@
 library(ggplot2)
 library(ggpubr)
 
+##Load data
 env.18S <- read.csv('Data/env_18S.csv', header = TRUE, row.names = 1)
 
-
+##Set colours for pH graphs
 mypal.pH <- colorRampPalette(c("#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b",
                                "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2"))
 
-## Sobs ##
+################### Sobs ##################
 
+##plot graph
 sobs.18S <-
   ggplot(env.18S, aes(pH, Sobs, color = pH)) + geom_point(size = 3) +
   xlab("pH") + ylab("Sobs") + ggtitle("18S") + theme_classic() + geom_smooth(
@@ -24,12 +26,16 @@ sobs.18S <-
 
 plot(sobs.18S)
 
+##model statistics
 summary(lm(env.18S, formula = Sobs ~ pH + I(pH^2)))
+
+##ANOVA
 anova(lm(env.18S, formula = Sobs ~ pH + I(pH^2)))
 
 
-## Shannon ##
+############### Shannon ###################
 
+##plot graph
 shan.18S <-
   ggplot(env.18S, aes(pH, Shan, color = pH)) + geom_point(size = 3) +
   xlab("pH") + ylab("Shannon") + ggtitle("18S") + theme_classic() + geom_smooth(
@@ -42,11 +48,12 @@ shan.18S <-
 
 plot(shan.18S)
 
+##model statistics
 summary(lm(env.18S, formula = Shan ~ pH + I(pH^2)))
+
+##ANOVA
 anova(lm(env.18S, formula = Shan ~ pH + I(pH^2)))
 
-
-
-#svg('4panel_sobs_shan.svg')
+##arrange graphs in one picture
 ggarrange(sobs.18S,shan.18S, nrow = 1, ncol = 2, common.legend = TRUE, legend = "right")
-#dev.off()
+
